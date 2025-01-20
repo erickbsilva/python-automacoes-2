3c5e886543e860a48755f067249069d4a0a1cbe5
Erick <erickbarretodasilva@gmail.com>
2025-01-20 11:45:06 -0300

    Coletando Vagas em Python II

--- 8-req_bsoup.py
+++ 8-req_bsoup.py
@@ -11,3 +11,12 @@ soup = BeautifulSoup(html_text, "lxml")
 jobs = soup.find_all("li", class_="clearfix job-bx wht-shd-bx")
 print(jobs[:3])
 print(len(jobs))
+
+# 2 - Estruturando informações
+for job in jobs[:20]:
+    name_company = job.find("h3", class_="joblist-comp-name").text.replace(" ", "")
+    skill = job.find("div", class_="srp-skills").text.replace(" ", "")
+    published_date = job.find("span", class_="sim-posted").span.text[7:]
+    print(name_company.strip())
+    print(skill)
+    print(published_date)
