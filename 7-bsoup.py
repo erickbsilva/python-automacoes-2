050196bc9eab86f658b9699fde28e1d36df81e99
Erick <erickbarretodasilva@gmail.com>
2025-01-20 11:17:52 -0300

    conhecendo mais o beatifulshop

--- 7-bsoup.py
+++ 7-bsoup.py
@@ -13,3 +13,12 @@ with open("pages/index.html", "r") as file_html:
     #  print(cursos)
     for curso in cursos:
         print(curso.text)
+
+    # 3 - Pegando mais informações
+    course_cards = soup.find_all("div", class_="card")
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+        print(course_name)
+        print(course_price)
+        print(f"{course_name} custa {course_price}")
