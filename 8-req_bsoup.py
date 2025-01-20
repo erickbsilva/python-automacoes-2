9c81ac329d98f18f4abb89e9d3fc5c4c3204c2f8
Erick <erickbarretodasilva@gmail.com>
2025-01-20 15:54:12 -0300

    exportando para csv

--- 8-req_bsoup.py
+++ 8-req_bsoup.py
@@ -12,6 +12,10 @@ jobs = soup.find_all("li", class_="clearfix job-bx wht-shd-bx")
 print(jobs[:3])
 print(len(jobs))
 
+companys = []
+skills = []
+pub_date = []
+
 
 # Função para remover espaços em branco
 def remover_espacos(lista):
@@ -22,11 +26,24 @@ def remover_espacos(lista):
 for job in jobs[:20]:
     name_company = job.find("h3", class_="joblist-comp-name").text.split()
     name_company = ",".join(name_company)
+
     skill = job.find("div", class_="srp-skills").text.split()
     skill = ",".join(skill)
-    published_date = job.find("span", class_="sim-posted").span.text[7:].split()
+
+    published_date = job.find("span", class_="sim-posted").span.text.split()
     published_date = ",".join(published_date)
 
     print(name_company)
     print(skill)
     print(published_date)
+
+    # 3 - Exportando Informações para CSV
+    companys.append(name_company.strip())
+    skills.append(skill.strip())
+    pub_date.append(published_date)
+
+python_vagas = pd.DataFrame()
+python_vagas["companys"] = companys
+python_vagas["skills"] = skills
+python_vagas["pub_date"] = pub_date
+python_vagas.to_csv("python_vagas.csv")
