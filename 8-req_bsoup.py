63fd1b5287678787e46b0559ca5da6c215e31b18
Erick <erickbarretodasilva@gmail.com>
2025-01-20 15:46:07 -0300

    Coletando Vagas em Python II - continuação

--- 8-req_bsoup.py
+++ 8-req_bsoup.py
@@ -12,11 +12,21 @@ jobs = soup.find_all("li", class_="clearfix job-bx wht-shd-bx")
 print(jobs[:3])
 print(len(jobs))
 
+
+# Função para remover espaços em branco
+def remover_espacos(lista):
+    return [termo.replace("\n", "") for termo in lista]
+
+
 # 2 - Estruturando informações
 for job in jobs[:20]:
-    name_company = job.find("h3", class_="joblist-comp-name").text.replace(" ", "")
-    skill = job.find("div", class_="srp-skills").text.replace(" ", "")
-    published_date = job.find("span", class_="sim-posted").span.text[7:]
-    print(name_company.strip())
+    name_company = job.find("h3", class_="joblist-comp-name").text.split()
+    name_company = ",".join(name_company)
+    skill = job.find("div", class_="srp-skills").text.split()
+    skill = ",".join(skill)
+    published_date = job.find("span", class_="sim-posted").span.text[7:].split()
+    published_date = ",".join(published_date)
+
+    print(name_company)
     print(skill)
     print(published_date)
